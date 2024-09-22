e210616776e77cec8cd6159057216c79c7b797fb
timurq98 <arelavis35@gmail.com>
2024-09-22 17:20:23 +0500

    Add error handling and string handling with command_line::get_arg
    Refactoring blockchain export
    
    Co-authored-by: Vladislav Averett <39224332+avrtt@users.noreply.github.com>

--- src/blockchain_utilities/blockchain_export.cpp
+++ src/blockchain_utilities/blockchain_export.cpp
@@ -40,145 +40,148 @@ namespace po = boost::program_options;
 using namespace cryptonote;
 using namespace epee;
 
-int main(int argc, char* argv[])
-{
-  TRY_ENTRY();
+int main(int argc, char* argv[]) {
+    TRY_ENTRY();
 
-  epee::string_tools::set_module_name_and_folder(argv[0]);
+    epee::string_tools::set_module_name_and_folder(argv[0]);
 
-  uint32_t log_level = 0;
-  uint64_t block_start = 0;
-  uint64_t block_stop = 0;
-  bool blocks_dat = false;
+    // Initialize log level and block boundaries
+    uint32_t log_level = 0;
+    uint64_t block_start = 0;
+    uint64_t block_stop = 0;
+    bool blocks_dat = false;
 
-  tools::on_startup();
+    tools::on_startup();
+    boost::filesystem::path output_file_path;
 
-  boost::filesystem::path output_file_path;
+    // Define command-line options
+    po::options_description desc_cmd_only("Command line options");
+    po::options_description desc_cmd_sett("Command line options and settings options");
+    
+    const auto arg_output_file = command_line::arg_descriptor<std::string>{"output-file", "Specify output file", "", true};
+    const auto arg_log_level = command_line::arg_descriptor<std::string>{"log-level", "0-4 or categories", ""};
+    const auto arg_block_start = command_line::arg_descriptor<uint64_t>{"block-start", "Start at block number", block_start};
+    const auto arg_block_stop = command_line::arg_descriptor<uint64_t>{"block-stop", "Stop at block number", block_stop};
+    const auto arg_blocks_dat = command_line::arg_descriptor<bool>{"blocksdat", "Output in blocks.dat format", blocks_dat};
 
-  po::options_description desc_cmd_only("Command line options");
-  po::options_description desc_cmd_sett("Command line options and settings options");
-  const command_line::arg_descriptor<std::string> arg_output_file = {"output-file", "Specify output file", "", true};
-  const command_line::arg_descriptor<std::string> arg_log_level  = {"log-level",  "0-4 or categories", ""};
-  const command_line::arg_descriptor<uint64_t> arg_block_start = {"block-start", "Start at block number", block_start};
-  const command_line::arg_descriptor<uint64_t> arg_block_stop = {"block-stop", "Stop at block number", block_stop};
-  const command_line::arg_descriptor<bool> arg_blocks_dat = {"blocksdat", "Output in blocks.dat format", blocks_dat};
+    // Add command line options
+    command_line::add_arg(desc_cmd_sett, cryptonote::arg_data_dir);
+    command_line::add_arg(desc_cmd_sett, arg_output_file);
+    command_line::add_arg(desc_cmd_sett, cryptonote::arg_testnet_on);
+    command_line::add_arg(desc_cmd_sett, cryptonote::arg_stagenet_on);
+    command_line::add_arg(desc_cmd_sett, arg_log_level);
+    command_line::add_arg(desc_cmd_sett, arg_block_start);
+    command_line::add_arg(desc_cmd_sett, arg_block_stop);
+    command_line::add_arg(desc_cmd_sett, arg_blocks_dat);
+    command_line::add_arg(desc_cmd_only, command_line::arg_help);
 
+    // Combine the options into a single description
+    po::options_description desc_options("Allowed options");
+    desc_options.add(desc_cmd_only).add(desc_cmd_sett);
 
-  command_line::add_arg(desc_cmd_sett, cryptonote::arg_data_dir);
-  command_line::add_arg(desc_cmd_sett, arg_output_file);
-  command_line::add_arg(desc_cmd_sett, cryptonote::arg_testnet_on);
-  command_line::add_arg(desc_cmd_sett, cryptonote::arg_stagenet_on);
-  command_line::add_arg(desc_cmd_sett, arg_log_level);
-  command_line::add_arg(desc_cmd_sett, arg_block_start);
-  command_line::add_arg(desc_cmd_sett, arg_block_stop);
-  command_line::add_arg(desc_cmd_sett, arg_blocks_dat);
+    // Parse command-line arguments
+    po::variables_map vm;
+    bool r = command_line::handle_error_helper(desc_options, [&]() {
+        po::store(po::parse_command_line(argc, argv, desc_options), vm);
+        po::notify(vm);
+        return true;
+    });
+    if (!r) return 1;
 
-  command_line::add_arg(desc_cmd_only, command_line::arg_help);
+    // Handle help argument
+    if (command_line::get_arg(vm, command_line::arg_help)) {
+        std::cout << "Monero '" << MONERO_RELEASE_NAME << "' (v" << MONERO_VERSION_FULL << ")" << std::endl << std::endl;
+        std::cout << desc_options << std::endl;
+        return 1;
+    }
 
-  po::options_description desc_options("Allowed options");
-  desc_options.add(desc_cmd_only).add(desc_cmd_sett);
+    // Configure logging
+    mlog_configure(mlog_get_default_log_path("monero-blockchain-export.log"), true);
+    const auto log_arg = command_line::get_arg(vm, arg_log_level);
+    mlog_set_log(!command_line::is_arg_defaulted(vm, arg_log_level) ? log_arg.c_str() : (std::to_string(log_level) + ",bcutil:INFO").c_str());
 
-  po::variables_map vm;
-  bool r = command_line::handle_error_helper(desc_options, [&]()
-  {
-    po::store(po::parse_command_line(argc, argv, desc_options), vm);
-    po::notify(vm);
-    return true;
-  });
-  if (! r)
-    return 1;
+    // Retrieve block start and stop arguments
+    block_start = command_line::get_arg(vm, arg_block_start);
+    block_stop = command_line::get_arg(vm, arg_block_stop);
 
-  if (command_line::get_arg(vm, command_line::arg_help))
-  {
-    std::cout << "Monero '" << MONERO_RELEASE_NAME << "' (v" << MONERO_VERSION_FULL << ")" << ENDL << ENDL;
-    std::cout << desc_options << std::endl;
-    return 1;
-  }
+    LOG_PRINT_L0("Starting...");
 
-  mlog_configure(mlog_get_default_log_path("monero-blockchain-export.log"), true);
-  if (!command_line::is_arg_defaulted(vm, arg_log_level))
-    mlog_set_log(command_line::get_arg(vm, arg_log_level).c_str());
-  else
-    mlog_set_log(std::string(std::to_string(log_level) + ",bcutil:INFO").c_str());
-  block_start = command_line::get_arg(vm, arg_block_start);
-  block_stop = command_line::get_arg(vm, arg_block_stop);
+    // Handle testnet and stagenet options
+    bool opt_testnet = command_line::get_arg(vm, cryptonote::arg_testnet_on);
+    bool opt_stagenet = command_line::get_arg(vm, cryptonote::arg_stagenet_on);
+    if (opt_testnet && opt_stagenet) {
+        std::cerr << "Can't specify more than one of --testnet and --stagenet" << std::endl;
+        return 1;
+    }
 
-  LOG_PRINT_L0("Starting...");
+    // Determine output format and file path
+    bool opt_blocks_dat = command_line::get_arg(vm, arg_blocks_dat);
+    std::string config_folder = command_line::get_arg(vm, cryptonote::arg_data_dir);
+    
+    output_file_path = command_line::has_arg(vm, arg_output_file)
+                        ? boost::filesystem::path(command_line::get_arg(vm, arg_output_file))
+                        : boost::filesystem::path(config_folder) / "export" / BLOCKCHAIN_RAW;
 
-  bool opt_testnet = command_line::get_arg(vm, cryptonote::arg_testnet_on);
-  bool opt_stagenet = command_line::get_arg(vm, cryptonote::arg_stagenet_on);
-  if (opt_testnet && opt_stagenet)
-  {
-    std::cerr << "Can't specify more than one of --testnet and --stagenet" << std::endl;
-    return 1;
-  }
-  bool opt_blocks_dat = command_line::get_arg(vm, arg_blocks_dat);
+    LOG_PRINT_L0("Export output file: " << output_file_path.string());
 
-  std::string m_config_folder;
+    // Initialize blockchain storage
+    LOG_PRINT_L0("Initializing source blockchain (BlockchainDB)");
+    std::unique_ptr<BlockchainAndPool> core_storage = std::make_unique<BlockchainAndPool>();
 
-  m_config_folder = command_line::get_arg(vm, cryptonote::arg_data_dir);
+    BlockchainDB* db = new_db();
+    if (!db) {
+        LOG_ERROR("Failed to initialize a database");
+        throw std::runtime_error("Failed to initialize a database");
+    }
 
-  if (command_line::has_arg(vm, arg_output_file))
-    output_file_path = boost::filesystem::path(command_line::get_arg(vm, arg_output_file));
-  else
-    output_file_path = boost::filesystem::path(m_config_folder) / "export" / BLOCKCHAIN_RAW;
-  LOG_PRINT_L0("Export output file: " << output_file_path.string());
+    LOG_PRINT_L0("database: LMDB");
 
-  // If we wanted to use the memory pool, we would set up a fake_core.
+    // Open the database
+    boost::filesystem::path folder(config_folder);
+    folder /= db->get_db_name();
+    const std::string filename = folder.string();
 
-  // Use Blockchain instead of lower-level BlockchainDB for two reasons:
-  // 1. Blockchain has the init() method for easy setup
-  // 2. exporter needs to use get_current_blockchain_height(), get_block_id_by_height(), get_block_by_hash()
-  LOG_PRINT_L0("Initializing source blockchain (BlockchainDB)");
-  std::unique_ptr<BlockchainAndPool> core_storage = std::make_unique<BlockchainAndPool>();
+    LOG_PRINT_L0("Loading blockchain from folder " << filename << " ...");
+    try {
+        db->open(filename, DBF_RDONLY);
+    } catch (const std::exception& e) {
+        LOG_PRINT_L0("Error opening database: " << e.what());
+        return 1;
+    }
 
-  BlockchainDB* db = new_db();
-  if (db == NULL)
-  {
-    LOG_ERROR("Failed to initialize a database");
-    throw std::runtime_error("Failed to initialize a database");
-  }
-  LOG_PRINT_L0("database: LMDB");
+    // Initialize blockchain
+    r = core_storage->blockchain.init(db, opt_testnet ? cryptonote::TESTNET : opt_stagenet ? cryptonote::STAGENET : cryptonote::MAINNET);
+    if (!r) {
+        LOG_ERROR("Failed to initialize source blockchain storage");
+        return 1;
+    }
 
-  boost::filesystem::path folder(m_config_folder);
-  folder /= db->get_db_name();
-  const std::string filename = folder.string();
+    // Check if blockchain is pruned
+    if (core_storage->blockchain.get_blockchain_pruning_seed() && !opt_blocks_dat) {
+        LOG_PRINT_L0("Blockchain is pruned, cannot export");
+        return 1;
+    }
 
-  LOG_PRINT_L0("Loading blockchain from folder " << filename << " ...");
-  try
-  {
-    db->open(filename, DBF_RDONLY);
-  }
-  catch (const std::exception& e)
-  {
-    LOG_PRINT_L0("Error opening database: " << e.what());
-    return 1;
-  }
-  r = core_storage->blockchain.init(db, opt_testnet ? cryptonote::TESTNET : opt_stagenet ? cryptonote::STAGENET : cryptonote::MAINNET);
+    LOG_PRINT_L0("Source blockchain storage initialized OK");
+    LOG_PRINT_L0("Exporting blockchain raw data...");
 
-  if (core_storage->blockchain.get_blockchain_pruning_seed() && !opt_blocks_dat)
-  {
-    LOG_PRINT_L0("Blockchain is pruned, cannot export");
-    return 1;
-  }
+    // Export blockchain data
+    if (opt_blocks_dat) {
+        BlocksdatFile blocksdat;
+        r = blocksdat.store_blockchain_raw(&core_storage->blockchain, nullptr, output_file_path, block_stop);
+    } else {
+        BootstrapFile bootstrap;
+        r = bootstrap.store_blockchain_raw(&core_storage->blockchain, nullptr, output_file_path, block_start, block_stop);
+    }
 
-  CHECK_AND_ASSERT_MES(r, 1, "Failed to initialize source blockchain storage");
-  LOG_PRINT_L0("Source blockchain storage initialized OK");
-  LOG_PRINT_L0("Exporting blockchain raw data...");
+    if (!r) {
+        LOG_ERROR("Failed to export blockchain raw data");
+        return 1;
+    }
 
-  if (opt_blocks_dat)
-  {
-    BlocksdatFile blocksdat;
-    r = blocksdat.store_blockchain_raw(&core_storage->blockchain, NULL, output_file_path, block_stop);
-  }
-  else
-  {
-    BootstrapFile bootstrap;
-    r = bootstrap.store_blockchain_raw(&core_storage->blockchain, NULL, output_file_path, block_start, block_stop);
-  }
-  CHECK_AND_ASSERT_MES(r, 1, "Failed to export blockchain raw data");
-  LOG_PRINT_L0("Blockchain raw data exported OK");
-  return 0;
+    LOG_PRINT_L0("Blockchain raw data exported OK");
+    return 0;
 
-  CATCH_ENTRY("Export error", 1);
+    CATCH_ENTRY("Export error", 1);
 }
+
